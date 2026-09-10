1b73e3c704ab097182f01c714fb8b0a0093a16e3
gabrielcastilho7 <gabriel.palhano.passos@escola.pr.gov.br>
2026-09-10 09:54:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const numeroSenha = Document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMinusculas =  'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
